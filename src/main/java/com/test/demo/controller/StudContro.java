d5b0e8cbfefc39dbf6feef1974cc33ba9d70bf4c
Shady Idrissi <chadiidac@gmail.com>
2024-11-23 22:04:08 +0100

    new

--- src/main/java/com/test/demo/controller/StudContro.java
+++ src/main/java/com/test/demo/controller/StudContro.java
@@ -1,12 +1,15 @@
 package com.test.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.demo.modules.Student;
 import com.test.demo.service.StudService;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/student")
@@ -15,8 +18,8 @@ public class StudContro {
     @Autowired
     private StudService studService;
 
-    @RequestMapping("/get_student")
-    public Student getUser(@RequestParam Integer id){
+    @RequestMapping("/get_student/{id}")
+    public Student getUser(@PathVariable Integer id){
         return studService.getUser(id);
     }
 }
